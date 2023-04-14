6b265cb98da7275903a3eaccf100e5cb4c4066fb
BelkevichAlexsander <jasonstyler58@gmail.com>
2023-04-14 16:22:14 +0300

    update logic database v1.1

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-import database.db
+import database.Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = database.db.Base.metadata
+target_metadata = database.Base.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
